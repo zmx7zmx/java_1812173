697c43f2bd1967d80916992480202f8ec6ddcb25
joshc81 <jchew1@sheffield.ac.uk>
2017-12-18 15:46:15 +0000

    Adding lego robot files
    I have made a few changes to the original lego robot football file. I'm trying to adjust the timings in order to get the robot to follow the black line (see comments). The catcher now works when pressing space bar and the same motor reverses when clicking the "r" key.

---
+++ ShefRobot/setupConsole.bat
@@ -0,0 +1,3 @@
+@echo OFF
+::C:\Windows\CCM\VAppLauncher.exe /launch "Java SDK 8 8.0.250.18"
+set CLASSPATH=.;bluecove-2.1.0.jar;ShefRobot.jar;dbusjava.jar;ev3classes.jar;%classpath%
---
+++ ShefRobot/setupConsole.sh
@@ -0,0 +1,2 @@
+# setupConsole.sh
+export CLASSPATH=.:bluecove-2.1.0.jar:ShefRobot.jar:dbusjava.jar:ev3classes.jar:$CLASSPATH
